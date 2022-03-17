5763993720c9d8f72a15f9f5f6d9b42147653ad7
erdem6161 <erdem.demircioglu@gmail.com>
2022-03-17 18:30:42 +0300

    image add

--- src/main/java/com/ozelservisnoktasi/OzelServisNoktasiApplication.java
+++ src/main/java/com/ozelservisnoktasi/OzelServisNoktasiApplication.java
@@ -3,7 +3,11 @@ package com.ozelservisnoktasi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
+
 public class OzelServisNoktasiApplication {
 
     public static void main(String[] args) {
